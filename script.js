a3254ef4ab9c84397bbadbe109f12be9c8d7e642
José M. Pérez <jmperez1985@gmail.com>
2016-08-08 08:52:31 +0200

    Lint code

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+/* global SpotifyWebApi */
 /*
   The code for finding out the BPM / tempo is taken from this post:
   http://tech.beatport.com/2014/web-audio/beat-detection-using-web-audio/
  */
+
+'use strict';
+
 var spotifyApi = new SpotifyWebApi();
 spotifyApi.getToken().then(function(response) {
   spotifyApi.setAccessToken(response.token);
@@ -46,8 +50,102 @@ playButton.addEventListener('click', function() {
 
 result.style.display = 'none';
 
-document.querySelector('form').addEventListener('submit', function(e) {
-  e.preventDefault();
+function getPeaks(data) {
+
+  // What we're going to do here, is to divide up our audio into parts.
+
+  // We will then identify, for each part, what the loudest sample is in that
+  // part.
+
+  // It's implied that that sample would represent the most likely 'beat'
+  // within that part.
+
+  // Each part is 0.5 seconds long - or 22,050 samples.
+
+  // This will give us 60 'beats' - we will only take the loudest half of
+  // those.
+
+  // This will allow us to ignore breaks, and allow us to address tracks with
+  // a BPM below 120.
+
+  var partSize = 22050,
+      parts = data[0].length / partSize,
+      peaks = [];
+
+  for (var i = 0; i < parts; i++) {
+    var max = 0;
+    for (var j = i * partSize; j < (i + 1) * partSize; j++) {
+      var volume = Math.max(Math.abs(data[0][j]), Math.abs(data[1][j]));
+      if (!max || (volume > max.volume)) {
+        max = {
+          position: j,
+          volume: volume
+        };
+      }
+    }
+    peaks.push(max);
+  }
+
+  // We then sort the peaks according to volume...
+
+  peaks.sort(function(a, b) {
+    return b.volume - a.volume;
+  });
+
+  // ...take the loundest half of those...
+
+  peaks = peaks.splice(0, peaks.length * 0.5);
+
+  // ...and re-sort it back based on position.
+
+  peaks.sort(function(a, b) {
+    return a.position - b.position;
+  });
+
+  return peaks;
+}
+
+function getIntervals(peaks) {
+
+  // What we now do is get all of our peaks, and then measure the distance to
+  // other peaks, to create intervals.  Then based on the distance between
+  // those peaks (the distance of the intervals) we can calculate the BPM of
+  // that particular interval.
+
+  // The interval that is seen the most should have the BPM that corresponds
+  // to the track itself.
+
+  var groups = [];
+
+  peaks.forEach(function(peak, index) {
+    for (var i = 1; (index + i) < peaks.length && i < 10; i++) {
+      var group = {
+        tempo: (60 * 44100) / (peaks[index + i].position - peak.position),
+        count: 1
+      };
+
+      while (group.tempo < 90) {
+        group.tempo *= 2;
+      }
+
+      while (group.tempo > 180) {
+        group.tempo /= 2;
+      }
+
+      group.tempo = Math.round(group.tempo);
+
+      if (!(groups.some(function(interval) {
+        return (interval.tempo === group.tempo ? interval.count++ : 0);
+      }))) {
+        groups.push(group);
+      }
+    }
+  });
+  return groups;
+}
+
+document.querySelector('form').addEventListener('submit', function(formEvent) {
+  formEvent.preventDefault();
   result.style.display = 'none';
   spotifyApi.searchTracks(
     queryInput.value.trim(), {limit: 1})
@@ -114,8 +212,9 @@ document.querySelector('form').addEventListener('submit', function(e) {
           var svg = document.querySelector('#svg');
           svg.innerHTML = '';
           var svgNS = 'http://www.w3.org/2000/svg';
+          var rect;
           peaks.forEach(function(peak) {
-            var rect = document.createElementNS(svgNS, 'rect');
+            rect = document.createElementNS(svgNS, 'rect');
             rect.setAttributeNS(null, 'x', (100 * peak.position / buffer.length) + '%');
             rect.setAttributeNS(null, 'y', 0);
             rect.setAttributeNS(null, 'width', 1);
@@ -123,7 +222,7 @@ document.querySelector('form').addEventListener('submit', function(e) {
             svg.appendChild(rect);
           });
 
-          var rect = document.createElementNS(svgNS, 'rect');
+          rect = document.createElementNS(svgNS, 'rect');
           rect.setAttributeNS(null, 'id', 'progress');
           rect.setAttributeNS(null, 'y', 0);
           rect.setAttributeNS(null, 'width', 1);
@@ -134,14 +233,14 @@ document.querySelector('form').addEventListener('submit', function(e) {
 
           var top = groups.sort(function(intA, intB) {
             return intB.count - intA.count;
-          }).splice(0,5);
+          }).splice(0, 5);
 
           text.innerHTML = '<div id="guess">Guess for track <strong>' + track.name + '</strong> by ' +
             '<strong>' + track.artists[0].name + '</strong> is <strong>' + Math.round(top[0].tempo) + ' BPM</strong>' +
             ' with ' + top[0].count + ' samples.</div>';
 
           text.innerHTML += '<div class="small">Other options are ' +
-            top.slice(1).map(function(group, index) {
+            top.slice(1).map(function(group) {
               return group.tempo + ' BPM (' + group.count + ')';
             }).join(', ') +
             '</div>';
@@ -151,8 +250,8 @@ document.querySelector('form').addEventListener('submit', function(e) {
                   tempo + ' BPM</div>';
           };
           spotifyApi.getAudioFeaturesForTrack(track.id)
-            .then(function(result) {
-              printENBPM(result.tempo);
+            .then(function(audioFeatures) {
+              printENBPM(audioFeatures.tempo);
             });
 
           result.style.display = 'block';
@@ -161,92 +260,3 @@ document.querySelector('form').addEventListener('submit', function(e) {
       request.send();
     });
 });
-
-
-function getPeaks(data) {
-
-  // What we're going to do here, is to divide up our audio into parts.
-
-  // We will then identify, for each part, what the loudest sample is in that
-  // part.
-
-  // It's implied that that sample would represent the most likely 'beat' 
-  // within that part.
-
-  // Each part is 0.5 seconds long - or 22,050 samples.
-
-  // This will give us 60 'beats' - we will only take the loudest half of
-  // those.
-
-  // This will allow us to ignore breaks, and allow us to address tracks with
-  // a BPM below 120.
-
-  var partSize = 22050,
-      parts = data[0].length / partSize, 
-      peaks = [];
-
-  for(i = 0; i < parts; i++) {
-      var max = 0;
-      for(j = i * partSize; j < (i + 1) * partSize; j++) {
-          volume = Math.max(Math.abs(data[0][j]), Math.abs(data[1][j]));
-          if(!max || (volume > max.volume)) {
-            max = {
-              position: j,
-              volume: volume
-            };
-          }
-      }
-      peaks.push(max);
-  }
-
-  // We then sort the peaks according to volume...
-
-  peaks.sort(function(a, b) {
-    return (b.volume > a.volume) ? 1 : ((a.volume > b.volume) ? -1 : 0);
-  });
-
-  // ...take the loundest half of those...
-
-  peaks = peaks.splice(0, peaks.length * 0.5);
-
-  // ...and re-sort it back based on position.
-
-  peaks.sort(function(a, b) {
-    return (a.position > b.position) ? 1 : ((b.position > a.position) ? -1 : 0);
-  });
-
-  return peaks;
-}
-
-function getIntervals(peaks) {
-
-  // What we now do is get all of our peaks, and then measure the distance to
-  // other peaks, to create intervals.  Then based on the distance between 
-  // those peaks (the distance of the intervals) we can calculate the BPM of
-  // that particular interval.
-
-  // The interval that is seen the most should have the BPM that corresponds
-  // to the track itself.
-
-  var groups = [];
-
-  peaks.forEach(function(peak, index) {
-    for(var i = 1; (index + i) < peaks.length && i < 10; i++) {
-      var group = {
-        tempo: (60 * 44100) / (peaks[index + i].position - peak.position),
-        count: 1
-      };
-      
-      while (group.tempo < 90)  group.tempo *= 2;
-      while (group.tempo > 180) group.tempo /= 2;
-      group.tempo = Math.round(group.tempo);
-
-      if (!(groups.some(function(interval) {
-        return (interval.tempo === group.tempo ? interval.count++ : 0);
-      }))) {
-        groups.push(group);
-      }
-    }
-  });
-  return groups;
-}
